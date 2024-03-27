9704bd1dcd74b164fc85c041d6f5525740165812
Jagadeesh Barbari <monk151947@gmail.com>
2024-03-28 00:19:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = process.env.PORT || 3003;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+})
+
+app.use('/', express.static(path.join(__dirname, './')));
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
+
